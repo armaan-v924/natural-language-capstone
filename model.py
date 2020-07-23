4d6b40ba857321503d4fc9f19dca86329ee3f462
GifStar <53477207+GifStar@users.noreply.github.com>
2020-07-23 14:36:13 -0400

    created a model-training file

---
+++ model.py
@@ -0,0 +1,16 @@
+"""train the model
+
+embed the caption
+
+embed the good image
+
+embed the bad image
+
+compute similarities (caption and good image, caption and bad image)
+
+compute loss and accuracy
+
+take optimization step"""
+
+import numpy as np
+import mygrad as mg
