e2ca492a557ab7119ba5af65b59f1ba96889afdb
GifStar <53477207+GifStar@users.noreply.github.com>
2020-07-24 10:32:26 -0400

    fixed a few errors on the training model.

--- model_train.py
+++ model_train.py
@@ -7,31 +7,31 @@ from mappings import Mappings
 import loss_and_acc_function as la
 import text_embedding as te
 import image_vector as iv
+import nn_setup as nn
 
 plotter,fig,ax = create_plot(metrics=["loss","accuracy"])
-model = Model(512,50) 
+model = nn.Model(512,50) 
 optim = SGD(model.parameters, learning_rate=0.001)
 
 plot_rate = 100
+train_data, test_data = nn.sample_data(model)
 
 for k in range(1000):
     
-    train_data, test_data = sample_data(model)
 
     wgood = te.text_embed(train_data[0],glove)
     wbad = te.text_embed(train_data[2],glove)
     good_caption = te.text_embed(train_data[1],glove)
     map = Mappings()
-    bad_caption = te.text_embed(get_captions_imgID(wbad))
+    bad_caption = te.text_embed(map.get_captions_imgID(wbad))
     dimg = iv.load_resnet()
     wimg = te.text_embed(dimg, glove)
     sgood = wimg * wgood
     sbad = wimg * wbad
     loss = la.loss(sgood,sbad)
-    acc = la.acc(prediction,truth)
 
 
-    plotter.set_train_batch({"loss":loss.item(), "accuracy":acc},batch_size=1, plot=False)
+    plotter.set_train_batch({"loss":loss.item()},batch_size=1, plot=False)
     if k % plot_rate== 0 and k > 0:
         plotter.set_train_epoch()
     
