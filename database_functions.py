0bd1c8677a5fe028bb4ea33f0a2b3e365efc4c65
AudreyL713 <2021leea@gmail.com>
2020-07-24 07:44:31 -0700

    add save model parameters

--- database_functions.py
+++ database_functions.py
@@ -46,3 +46,13 @@ def add_image(image_id):
     semantic_embeddings = get_embeddings(image_id) #after training
     database[tuple(image_vector)] = (image_id, semantic_embeddings)
     save_db(database)
+
+def save_model(self, path):
+    """Path to .npz file where model parameters will be saved."""
+    with open(path, "wb") as f:
+        np.savez(f, *(x.data for x in self.parameters))
+
+def load_model(self, path):
+    with open(path, "rb") as f:
+        for param, (name, array) in zip(self.parameters, np.load(f).items()):
+            param.data[:] = array
