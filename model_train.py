945d0075d570a7000f09237c8e38bcad86728204
GifStar <53477207+GifStar@users.noreply.github.com>
2020-07-24 11:56:15 -0400

    finalized model_train.py

--- model_train.py
+++ model_train.py
@@ -2,7 +2,7 @@
 from noggin import create_plot
 import mygrad as mg
 import numpy as np
-from mynn.optimizers.sgd import SGD
+from mynn.optimizers.SGD import SGD
 from mappings import Mappings
 import loss_and_acc_function as la
 import text_embedding as te
@@ -18,30 +18,72 @@ model = nn.Model(512,50)
 optim = SGD(model.parameters, learning_rate=0.001)
 
 plot_rate = 100
+map = Mappings()
 train_data, test_data = nn.sample_data(model)
+batch_size = 32
+resnet = iv.load_resnet()
+for epoch_rate in range(10000):
+    idxs = np.arange(len(train_data))
+    np.random.shuffle(idxs)
+    for batch_rate in range(len(train_data)//batch_size):
+        batch_indices = idxs[batch_rate*batch_size:(batch_rate+1)*batch_size]
+        batch = train_data[batch_indices]
+        dgood = []
+        dbad = []
+        wcaption = []
+        for x in batch:
+            dgoodtemp = iv.get_resnet_vector(x[0],resnet)
+            dbadtemp = iv.get_resnet_vector(x[2],resnet)
+            wcaptiontemp = te.text_embed(x[1],glove)
 
-for k in range(1000):
+            if dgoodtemp != 0 and dbadtemp != 0:
+                dgood.append(dgoodtemp)
+                dbad.append(dbadtemp)
+                wcaption.append(dgoodtemp)
+
+        wcaption = np.array(wcaption)
+        wgood = model(np.array(dgood))
+        wbad = model(np.array(dbad))
+        sgood = wcaption * wgood
+        sbad = wcaption * wbad
+        loss = la.loss(sgood,sbad)
+        loss.backward()
+        optim.step()
+        loss.null_gradients()
+
+
+        plotter.set_train_batch({"loss":loss.item()},batch_size=batch_size, plot=False)
+
+    for batch_rate in range(len(test_data)//batch_size):
+        batch_indices = idxs[batch_rate*batch_size:(batch_rate+1)*batch_size]
+        batch = test_data[batch_indices]
+        dgood = []
+        dbad = []
+        wcaption = []
+        for x in batch:
+            dgoodtemp = iv.get_resnet_vector(x[0],resnet)
+            dbadtemp = iv.get_resnet_vector(x[2],resnet)
+            wcaptiontemp = te.text_embed(x[1],glove)
+
+            if dgoodtemp != 0 and dbadtemp != 0:
+                dgood.append(dgoodtemp)
+                dbad.append(dbadtemp)
+                wcaption.append(dgoodtemp)
+
+        wcaption = np.array(wcaption)
+        wgood = model(np.array(dgood))
+        wbad = model(np.array(dbad))
+        sgood = wcaption * wgood
+        sbad = wcaption * wbad
+        loss = la.loss(sgood,sbad)
+        loss.backward()
+        loss.null_gradients()
+        
+        plotter.set_test_batch({"loss":loss.item()}, batch_size=batch_size, plot=False)
     
-
-    wgood = te.text_embed(train_data[0],glove)
-    wbad = te.text_embed(train_data[2],glove)
-    good_caption = te.text_embed(train_data[1],glove)
-    map = Mappings()
-    bad_caption = te.text_embed(map.get_captions_imgID(wbad))
-    dimg = iv.load_resnet()
-    wimg = te.text_embed(dimg, glove)
-    sgood = wimg * wgood
-    sbad = wimg * wbad
-    loss = la.loss(sgood,sbad)
-
-
-    plotter.set_train_batch({"loss":loss.item()},batch_size=1, plot=False)
-    if k % plot_rate== 0 and k > 0:
-        plotter.set_train_epoch()
-    
-    loss.backward()
-    optim.step()
-    loss.null_gradients()
+    if epoch_rate % plot_rate== 0 and epoch_rate > 0:
+            plotter.set_train_epoch()
+            plotter.set_test_epoch()
     
 model.save_model("trained_parameters.npz")
 
