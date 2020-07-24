7e651fbeca059b716858afe3f5ca4b7fd11cbd7c
GifStar <53477207+GifStar@users.noreply.github.com>
2020-07-24 12:26:20 -0400

    resnet

--- model_train.py
+++ model_train.py
@@ -19,9 +19,9 @@ optim = SGD(model.parameters, learning_rate=0.001)
 
 plot_rate = 100
 map = Mappings()
-train_data, test_data = nn.sample_data(map)
 batch_size = 32
 resnet = iv.load_resnet()
+train_data, test_data = nn.sample_data(resnet)
 for epoch_rate in range(10000):
     idxs = np.arange(len(train_data))
     np.random.shuffle(idxs)
